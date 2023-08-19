8493e7a6968ab9e017835494dbad48729d46461b
georgehetrelezis <georgehetrelezis@gmail.com>
2023-08-19 15:37:54 +1000

    [Code]] Upload progress for form.

---
+++ test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("C:/Users/George/Documents/.Projects/quantium-govhack-2023/finaldata.csv")
+data.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
