73a075bb8913059766093a1aee5b5bb36486613a
georgehetrelezis <georgehetrelezis@gmail.com>
2023-08-19 21:16:13 +1000

    [Code] Add Maps embed to Roundabout and update nav logo

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("C:/Users/George/Documents/.Projects/quantium-govhack-2023/finaldata.csv")
-data.head()
+dframe = pd.read_csv("C:/Users/George/Documents/.Projects/quantium-govhack-2023/finaldata.csv")
+dframe.head()
 
-
+from statsmodels.formula.api import logit
+logistic = logit("Total.Score ~ Rural", 
+                 data = dframe).fit()
+print(logistic)
 
 
 
